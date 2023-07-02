5eecd03debd8302cf316681ec8f665b0a2d4a515
Jessica Thompson <116688161+jessicathompson88@users.noreply.github.com>
2023-07-02 14:45:18 -0500

    Updated CuisineOptionsController see comments

---
+++ Application/src/main/java/com/kenzie/appserver/controller/FrontendCreateRecipePageOptionsController.java
@@ -0,0 +1,61 @@
+package com.kenzie.appserver.controller;
+
+import com.kenzie.appserver.repositories.model.Enums;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:63342")
+public class FrontendCreateRecipePageOptionsController {
+
+    @GetMapping("/cuisineOptions")
+    public String getCuisineOptions() {
+        Enums.Cuisine[] cuisines = Enums.Cuisine.values();
+
+        StringBuilder jsonBuilder = new StringBuilder("[");
+        for (int i = 0; i < cuisines.length; i++) {
+            jsonBuilder.append("\"").append(cuisines[i].toString()).append("\"");
+            if (i < cuisines.length - 1) {
+                jsonBuilder.append(",");
+            }
+        }
+        jsonBuilder.append("]");
+
+        return jsonBuilder.toString();
+    }
+
+    @GetMapping("/dietaryRestrictionOptions")
+    public String getDietaryRestrictionOptions() {
+        Enums.DietaryRestrictions[] dietaryRestrictions = Enums.DietaryRestrictions.values();
+
+        StringBuilder jsonBuilder = new StringBuilder("[");
+        for (int i = 0; i < dietaryRestrictions.length; i++) {
+            jsonBuilder.append("\"").append(dietaryRestrictions[i].toString()).append("\"");
+            if (i < dietaryRestrictions.length - 1) {
+                jsonBuilder.append(",");
+            }
+        }
+        jsonBuilder.append("]");
+
+        return jsonBuilder.toString();
+    }
+
+    @GetMapping("/quantityTypeOptions")
+    public String getQuantityTypeOptions() {
+        Enums.QuantityType[] quantityTypes = Enums.QuantityType.values();
+
+        StringBuilder jsonBuilder = new StringBuilder("[");
+        for (int i = 0; i < quantityTypes.length; i++) {
+            jsonBuilder.append("\"").append(quantityTypes[i].toString()).append("\"");
+            if (i < quantityTypes.length - 1) {
+                jsonBuilder.append(",");
+            }
+        }
+        jsonBuilder.append("]");
+
+        return jsonBuilder.toString();
+    }
+}
