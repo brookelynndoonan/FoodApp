bf3ace8213a6e076fdc8e770f73b4366308cac93
Jessica Thompson <116688161+jessicathompson88@users.noreply.github.com>
2023-07-03 00:57:26 -0500

    Trying to help get controllers working after I trashed everything with enums lol

--- Application/src/main/java/com/kenzie/appserver/service/model/Recipe.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/Recipe.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Recipe {
-    private final String id;
+    private String id;
     private final String title;
     private final String cuisine;
     private final String description;
@@ -29,7 +29,7 @@ public class Recipe {
     }
 
     public String getCuisine() {
-        return cuisine;
+        return cuisine.toUpperCase().replace(" ", "_");
     }
 
     public String getDescription() {
@@ -37,7 +37,7 @@ public class Recipe {
     }
 
     public String getDietaryRestrictions() {
-        return dietaryRestrictions;
+        return dietaryRestrictions.toUpperCase().replace(" ", "_");
     }
 
     public boolean hasDietaryRestrictions() {
@@ -61,5 +61,9 @@ public class Recipe {
 
     public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
