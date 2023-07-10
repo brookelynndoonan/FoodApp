ea7d35616d83a41fb45b7caf560bfc7eab39a53f
Jessica Thompson <116688161+jessicathompson88@users.noreply.github.com>
2023-07-09 20:13:06 -0500

    removed ignore case from findbycuisine in repository

--- Application/src/main/java/com/kenzie/appserver/repositories/RecipeRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/RecipeRepository.java
@@ -1,5 +1,4 @@
 package com.kenzie.appserver.repositories;
-//for merge
 import com.kenzie.appserver.repositories.model.RecipeRecord;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
