d27997b0f98146cd8c8af292c80d0ebb4c4bee2a
Jessica Thompson <116688161+jessicathompson88@users.noreply.github.com>
2023-07-12 10:15:07 -0500

    Fixed CreateRecipe to pull options and submit all fields correctly to the backend

--- Application/src/main/java/com/kenzie/appserver/repositories/model/Enums.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/Enums.java
@@ -3,43 +3,65 @@ package com.kenzie.appserver.repositories.model;
 public class Enums {
 
     public enum Cuisine {
-        ITALIAN,
-        MEXICAN,
-        CHINESE,
-        INDIAN,
-        FRENCH,
-        JAPANESE,
-        AMERICAN,
-        ASIAN,
-        THAI
+        ITALIAN("Italian"),
+        MEXICAN("Mexican"),
+        INDIAN("Indian"),
+        FRENCH("French"),
+        JAPANESE("Japanese"),
+        AMERICAN("American"),
+        ASIAN("Asian"),
+        THAI("Thai"),
+        CHINESE("Chinese");
+
+        private final String displayName;
+
+        Cuisine(String displayName) {
+            this.displayName = displayName;
+        }
+        public String getDisplayName() {
+            return displayName;
+        }
 
         // Add more cuisine types as needed
     }
     public enum DietaryRestrictions {
-        VEGAN,
-        VEGETARIAN,
-        GLUTEN_FREE,
-        DAIRY_FREE,
-        NUT_FREE,
+        VEGAN("Vegan"),
+        VEGETARIAN("Vegetarian"),
+        GLUTEN_FREE("Gluten Free"),
+        DAIRY_FREE("Dairy Free"),
+        NUT_FREE("Nut Free");
+
+        private final String displayName;
+        DietaryRestrictions(String displayName) {
+            this.displayName = displayName;
+        }
+        public String getDisplayName() {
+            return displayName;
+        }
         // Add more dietary restrictions as needed
     }
 
     public enum QuantityType {
-        GRAMS,
-        KILOGRAMS,
-        MILLILITERS,
-        LITERS,
-        TEASPOONS,
-        TABLESPOONS,
-        CUPS,
-        CUP,
-        PIECES,
-        PACKS,
-        POUND,
-        POUNDS,
-        OZ,
-        CLOVES,
-        BULB
+        GRAMS("Gram(s)"),
+        KILOGRAMS("Kilogram(s)"),
+        MILLILITERS("Milliliter(s)"),
+        LITERS("Liter(s)"),
+        TEASPOONS("Teaspoon(s)"),
+        TABLESPOONS("Tablespoon(s)"),
+        CUPS("Cup(s)"),
+        PIECES("Piece(s)"),
+        PACKS("Pack(s)"),
+        POUNDS("Pound(s)"),
+        OZ("Oz"),
+        CLOVES("Clove(s)");
+
+        private final String displayName;
+        QuantityType(String displayName) {
+            this.displayName = displayName;
+        }
+        public String getDisplayName() {
+            return displayName;
+        }
     }
 
 }
