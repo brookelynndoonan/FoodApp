0a21276b4883c43193d7b779ce2504c9bea1c72e
brookelynndoonan <brookelynn.doonan@mykenzie.snhu.edu>
2023-07-13 23:31:45 -0600

    MC

--- Application/src/main/java/com/kenzie/appserver/repositories/model/Enums.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/Enums.java
@@ -2,46 +2,44 @@ package com.kenzie.appserver.repositories.model;
 
 public class Enums {
 
-    public enum Cuisine {
+        public enum Cuisine {
+                ITALIAN,
+                MEXICAN,
+                INDIAN,
+                FRENCH,
+                JAPANESE,
+                AMERICAN,
+                ASIAN,
+                THAI,
+                CHINESE
 
-        ITALIAN,
-        MEXICAN,
-        INDIAN,
-        FRENCH,
-        JAPANESE,
-        AMERICAN,
-        ASIAN,
-        THAI,
-        CHINESE
+                // Add more cuisine types as needed
+        }
+        public enum DietaryRestrictions {
+                VEGAN,
+                VEGETARIAN,
+                GLUTEN_FREE,
+                DAIRY_FREE,
+                NUT_FREE
 
-        // Add more cuisine types as needed
-    }
-    public enum DietaryRestrictions {
+                // Add more dietary restrictions as needed
+        }
 
-        VEGAN,
-        VEGETARIAN,
-        GLUTEN_FREE,
-        DAIRY_FREE,
-        NUT_FREE
+        public enum QuantityType {
+                GRAMS,
+                KILOGRAMS,
+                MILLILITERS,
+                LITERS,
+                TEASPOONS,
+                TABLESPOONS,
+                CUPS,
+                QUARTS,
+                GALLONS,
+                PIECES,
+                PACKS,
+                POUNDS,
+                OZ
 
-        // Add more dietary restrictions as needed
-    }
-
-    public enum QuantityType {
-
-        GRAMS,
-        KILOGRAMS,
-        MILLILITERS,
-        LITERS,
-        TEASPOONS,
-        TABLESPOONS,
-        CUPS,
-        QUARTS,
-        GALLONS,
-        PIECES,
-        PACKS,
-        POUNDS,
-        OZ
-    }
+        }
 
 }
