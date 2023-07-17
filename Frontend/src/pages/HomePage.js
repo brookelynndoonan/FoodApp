35f1ce0f6badb7b87b388daafbc90dd9770f2cbf
Jessica Thompson <116688161+jessicathompson88@users.noreply.github.com>
2023-07-16 23:29:48 -0500

    MC

--- Frontend/src/pages/HomePage.js
+++ Frontend/src/pages/HomePage.js
@@ -1,7 +1,7 @@
 
-import BaseClass from "baseClass.js";
+import BaseClass from "../util/baseClass";
 import HomeClient from "../api/HomeClient"; // Import the client class for making API requests
-import DataStore from "./DataStore";
+import DataStore from "../util/DataStore";
 class HomePage extends BaseClass {
     constructor() {
         super();
